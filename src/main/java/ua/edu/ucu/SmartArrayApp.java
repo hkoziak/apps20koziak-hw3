4c1e88463f38587539273add52891d79fb7978b8
Halyna Koziak <galiakozyak@gmail.com>
2020-11-26 15:54:51 +0200

    fix typo (imports)

--- src/main/java/ua/edu/ucu/SmartArrayApp.java
+++ src/main/java/ua/edu/ucu/SmartArrayApp.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import ua.edu.ucu.functions.MyComparator;
 import ua.edu.ucu.functions.MyFunction;
 import ua.edu.ucu.functions.MyPredicate;
-import ua.edu.ucu.smartarr.SmartArray;
+import ua.edu.ucu.smartarr.*;
 
 public class SmartArrayApp {
 
